c124ff8cc1419232720d2d66d3e76830a1ebb78e
rabaou-ibrahim <114422669+rabaou-ibrahim@users.noreply.github.com>
2023-01-16 17:36:13 +0100

    Add files via upload
    3 premiers jobs. Javascript affichage d'une popup avec/sans fichier externe, puis dans la console

---
+++ job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Popup JavaScript</h2>
+
+<button onclick="myFunction()">Cliquez ici</button>
+
+<script>
+function myFunction() {
+  alert("Hello JavaScript!");
+}
+</script>
+
+</body>
+</html>
---
+++ job02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Popup JavaScript en utilisant un fichier externe</h2>
+
+<head>
+<script src="script.js"></script>
+<title>JavaScript</title>
+</head>
+
+<button onclick="myFunction()">Cliquez ici</button>
+
+</body>
+</html>
