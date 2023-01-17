861c46bd82db64d5dc8222a29df134749589ee09
rabaou-ibrahim <114422669+rabaou-ibrahim@users.noreply.github.com>
2023-01-17 17:02:15 +0100

    Add files via upload
    job01 fini

---
+++ jour02/job01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Jour02.Job01</h2>
+
+<head>
+<script src="script.js"></script>
+<title>Jour02.Job01</title>
+</head>
+
+<article id = "article" onclick="citation()">
+<i>La vie a beaucoup plus d'imagination que nous</i>
+</article>
+
+<br>
+
+<button id = "button" onclick="citation()">Cliquez ici</button>
+
+</body>
+</html>
